8362295316ac14854b8f6c6e33c1a77462bcbce7
hansakarathnayake <hndk2002@gmail.com>
2024-11-11 22:54:26 +0530

    Build: dashboard form

---
+++ src/controller/MainWindowFormController.java
@@ -0,0 +1,35 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
+public class MainWindowFormController {
+
+    public AnchorPane contextOutlet;
+
+    public void dashboardOnAction(ActionEvent actionEvent) {
+
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/DashboardForm.fxml"));
+            Parent dashboardView = loader.load();
+
+            contextOutlet.getChildren().clear();
+            contextOutlet.getChildren().add(dashboardView);
+
+            contextOutlet.setTopAnchor(dashboardView, 0.0);
+            contextOutlet.setLeftAnchor(dashboardView, 0.0);
+            contextOutlet.setRightAnchor(dashboardView, 0.0);
+            contextOutlet.setBottomAnchor(dashboardView, 0.0);
+
+
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+}
